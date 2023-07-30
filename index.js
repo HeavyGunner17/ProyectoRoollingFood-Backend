ec0b502a332bdacc172da77b0f427345e1ea741d
Nahuel Benitez <jorgenahuelbenitez07@gmail.com>
2023-07-30 16:32:33 -0300

    agrego ruta de productos

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { dbConnection } = require('./utils/conectionDB');
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const app = express();
+const port = 3000;
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+dbConnection();
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.use('/products', require('./routes/productRouter'));
+//app.use('/category/:categoryId', require('./routes/categoryRouter'));
+//app.use('/users/', require('./routes/userRouter'));
+
+
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
